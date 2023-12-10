5ddcb19e721d5f7d8f436f9146ab3420eae57b59
HussBTWYT <121028340+HussBTWYT@users.noreply.github.com>
2023-12-10 10:56:54 +0000

    Create main.py

---
+++ examples/main.py
@@ -0,0 +1,10 @@
+import requests, json
+
+url = "https://hussbtwjokesapi.pythonanywhere.com/api/randomjoke"
+params = "joke"
+
+response = requests.get(url, params)
+
+data = json.loads(response.text)
+
+print(data["joke"])
